259ac7e549c374b268fbbcee736ccd69151272b9
princessibtihaj <ibtihajprincess@gmail.com>
2026-04-20 20:27:28 -0500

    Changed all the translate to recipe

--- website/views.py
+++ website/views.py
@@ -8,11 +8,11 @@ main_blueprint = Blueprint('main', __name__)
 OPENAI_KEY = os.environ.get('OPEN_AI_KEY')
 
 
-def translateAPI(to_translate, toSlang=False):
+def generateRecipeAPI(recipe_request):
 
     content_text = (
         "Create a clear recipe based on this user request: "
-        + to_translate
+        + recipe_request
         + ". Return the recipe with a short title, an ingredients list, and numbered steps."
     )
 
@@ -37,16 +37,16 @@ def main_page():
     return render_template('index.html')
     
 
-@main_blueprint.route('/translate', methods=['GET', 'POST'])
-def translate():
+@main_blueprint.route('/recipe', methods=['GET', 'POST'])
+def recipe():
 
     if request.method == 'POST':
 
         # we got the request as a json object
         data = request.get_json()       
         # we use a function to communicate with the API
-        translation = translateAPI(data['input'])
+        recipe = generateRecipeAPI(data['input'])
 
         # we return the result as a json object, notice no render_template
-        result = {"translatedText": translation}
+        result = {"recipeText": recipe}
         return result
